2bd0e37679119713e6b6ca4f3559ae203591a6f3
Yasunori Takahashi <hibriiiiidge@gmail.com>
2019-04-03 23:14:33 +0900

    Createable task by command

--- todo/lib/todo/command/options.rb
+++ todo/lib/todo/command/options.rb
@@ -21,6 +21,8 @@ module Todo
         rescue OptionParser::MissingArgument, OptionParser::InvalidOption, ArgumentError => e
           abort e.message
         end
+
+        options
       end
 
       def self.create_sub_command_parsers(options)
@@ -29,26 +31,60 @@ module Todo
         end
 
         sub_command_parsers['create'] = OptionParser.new do |opt|
+          opt.banner = 'Usage: create <args>'
           opt.on('-n VAL', '--name=VAL', 'task name') { |v| options[:name] = v }
           opt.on('-c VAL', '--content=VAL', 'task content') { |v| options[:content] = v }
+          opt.on_tail('-h', '--help', 'Show this message') { |v| help_sub_command(opt) }
         end
 
         sub_command_parsers['list'] = OptionParser.new do |opt|
+          opt.banner = 'Usage: list <args>'
           opt.on('-s VAL', '--status=VAL', 'list status') { |v| options[:status] = v }
+          opt.on_tail('-h', '--help', 'Show this message') { |v| help_sub_command(opt) }
         end
 
         sub_command_parsers['update'] = OptionParser.new do |opt|
+          opt.banner = 'Usage: update id <args>'
           opt.on('-n VAL', '--name=VAL', 'update name') { |v| options[:name] = v }
           opt.on('-c VAL', '--content=VAL', 'update content') { |v| options[:content] = v }
           opt.on('-s VAL', '--status=VAL', 'update status') { |v| options[:status] = v }
+          opt.on_tail('-h', '--help', 'Show this message') { |v| help_sub_command(opt) }
         end
 
         sub_command_parsers['delete'] = OptionParser.new do |opt|
+          opt.banner = 'Usage: delete id'
+          opt.on_tail('-h', '--help', 'Show this message') { |v| help_sub_command(opt) }
         end
+
+        sub_command_parsers
+      end
+
+      def self.help_sub_command(parser)
+        puts parser.help
+        exit
       end
 
       def self.create_command_parser
         OptionParser.new do |opt|
+          sub_command_help = [
+            { name: 'create -n name -c content', summary: 'Create Todo Task' },
+            { name: 'update id -n name -c content -s status', summary: 'Update Todo Task' },
+            { name: 'list -s status', summary: 'List Todo Task' },
+            { name: 'delete id', summary: 'Delete Todo Task' }
+          ]
+
+          opt.banner = "Update: #{opt.program_name} [-h|--help][-v|--version] <command> [<args>]"
+          opt.separator ''
+          opt.separator "#{opt.program_name} Available Commands:"
+          sub_command_help.each do |command|
+            opt.separator [opt.summary_indent, command[:name].ljust(40), command[:summary]].join(' ')
+          end
+
+          opt.on_head('-h', '--help', 'Show this message') do |v|
+            puts opt.help
+            exit
+          end
+
           opt.on_head('-v', '--version', 'Show program version') do |v|
             opt.version = Todo::VERSION
             puts opt.ver
@@ -56,6 +92,8 @@ module Todo
           end
         end
       end
+
+      private_class_method :create_sub_command_parsers, :create_command_parser, :help_sub_command
     end
   end
 end
